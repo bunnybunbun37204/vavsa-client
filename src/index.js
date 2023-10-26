e71cc5363f2a23b6b3770c0e1c3bdaa812b56bd0
Bunyawat Naunnak <bunyawat@localhost.localdomain>
2023-10-27 01:25:50 +0700

    update app.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
-ReactDOM.render(
+const root = document.getElementById('root');
+const rootContainer = createRoot(root);
+
+rootContainer.render(
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
-  document.getElementById('root')
+  </BrowserRouter>
 );
